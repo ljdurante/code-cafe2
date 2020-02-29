76b5a1812aa3dd60cae4bfe8405fef4395ec3284
Lawrence Snead <Lawrence.Snead@gmail.com>
2020-02-29 09:59:29 -0500

    Drowning...

--- models/post.js
+++ models/post.js
@@ -10,15 +10,20 @@ module.exports = function(sequelize, DataTypes) {
       body: {
         type: DataTypes.TEXT,
         allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      category: {
-        type: DataTypes.STRING,
-        defaultValue: "Personal"
+        len: [1]
       }
     });
+  
+    Post.associate = function(models) {
+      // We're saying that a Post should belong to an Author
+      // A Post can't be created without an Author due to the foreign key constraint
+      Post.belongsTo(models.Author, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
     return Post;
   };
   
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -46,98 +46,67 @@ module.exports = function(app) {
       });
     }
   });
-
-  app.get("/api/posts", function (req, res) {
-    // Add sequelize code to find all posts, and return them to the user with res.json
-    db.Post.findAll().then(dbPost => {res.json(dbPost)});
-  });
-
-  // Get route for returning posts of a specific category
-  app.get("/api/posts/category/:category", function (req, res) {
-    // Add sequelize code to find all posts where the category is equal to req.params.category,
-    // return the result to the user with res.json
-    db.Post.findAll({where: {category: req.params.category}}).then(dbPost => {res.json(dbPost)});
+  // GET route for getting all of the posts
+  app.get("/api/posts", function(req, res) {
+    var query = {};
+    if (req.query.author_id) {
+      query.AuthorId = req.query.author_id;
+    }
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Author
+    db.Post.findAll({
+      where: query,
+      include: [db.Author]
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
   });
 
   // Get route for retrieving a single post
-  app.get("/api/posts/:id", function (req, res) {
-    // Add sequelize code to find a single post where the id is equal to req.params.id,
-    // return the result to the user with res.json
-    db.Post.findOne({where: {id: req.params.id}}).then(dbPost => {res.json(dbPost)});
+  app.get("/api/posts/:id", function(req, res) {
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Author
+    db.Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Author]
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
   });
 
   // POST route for saving a new post
-  app.post("/api/posts", function (req, res) {
-    // Add sequelize code for creating a post using req.body,
-    // then return the result using res.json
-    db.Post.create({
-      title: req.body.title,
-      body: req.body.body,
-      category: req.body.category
-    }).then(dbPost => {res.json(dbPost)});
+  app.post("/api/posts", function(req, res) {
+    db.Post.create(req.body).then(function(dbPost) {
+      res.json(dbPost);
+    });
   });
 
   // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function (req, res) {
-    // Add sequelize code to delete a post where the id is equal to req.params.id, 
-    // then return the result to the user using res.json
+  app.delete("/api/posts/:id", function(req, res) {
     db.Post.destroy({
       where: {
         id: req.params.id
       }
-    }).then(dbPost => { res.json(dbPost)});
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
   });
 
   // PUT route for updating posts
-  app.put("/api/posts", function (req, res) {
-    // Add code here to update a post using the values in req.body, where the id is equal to
-    // req.body.id and return the result to the user using res.json
-    db.Post.update({
-      title: req.body.title,
-      body: req.body.body,
-      category: req.body.category
-    }, { where: { id: req.body.id } }).then(dbPost => { res.json(dbPost) });
-  });
-
-  app.get("/api/authors", function(req, res) {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-    db.Author.findAll({
-      include: [db.Post]
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+  app.put("/api/posts", function(req, res) {
+    db.Post.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbPost) {
+      res.json(dbPost);
     });
   });
-
-  app.get("/api/authors/:id", function(req, res) {
-    // Here we add an "include" property to our options in our findOne query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-    db.Author.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.Post]
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
-    });
-  });
-
-  app.post("/api/authors", function(req, res) {
-    db.Author.create(req.body).then(function(dbAuthor) {
-      res.json(dbAuthor);
-    });
-  });
-
-  app.delete("/api/authors/:id", function(req, res) {
-    db.Author.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
-    });
-  });
-
+  
 };
